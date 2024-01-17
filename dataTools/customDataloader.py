b84b684461f9d0adde4c96da846de8f6118312f1
Кильчуков Тимур Альбертович <tkilchukov@npomis.ru>
2024-01-17 17:30:11 +0300

    first

--- dataTools/customDataloader.py
+++ dataTools/customDataloader.py
@@ -1,7 +1,60 @@
+# import glob
+# import numpy as np
+# import time
+# import cv2
+# from PIL import Image
+# from torch.utils.data import DataLoader, Dataset
+# import torchvision.transforms as transforms
+# from utilities.customUtils import *
+# from dataTools.dataNormalization import *
+# from dataTools.customTransform import *
+# import os
+# class customDatasetReader(Dataset):
+#     def __init__(self, image_list, imagePath, height, width, transformation=True):
+#         self.image_list = image_list
+#         self.imagePath = imagePath
+#         self.transformLR = transforms
+#         self.imageH = height
+#         self.imageW = width
+#         self.normalize = transforms.Normalize(normMean, normStd)
+#         self.var = 0.1
+#         self.mean = 0.0
+#         self.pov = 0.3
+
+
+#     def __len__(self):
+#         return (len(self.image_list))
+    
+#     def __getitem__(self, i):
+
+
+
+#         self.transformHRGT = transforms.Compose([
+#                                                 transforms.ToTensor(),
+#                                                 self.normalize,
+#                                                 ])
+        
+#         self.gtImageFileName = self.imagePath + extractFileName(self.image_list[i])
+#         self.gtImage = Image.open(self.gtImageFileName)
+
+#         # Transforms Images for training 
+#         self.gtImageHR = self.transformHRGT(self.gtImage)
+        
+#         #Noise Modeling
+#         sigma = random.uniform(0, self.var ** self.pov)
+#         noiseModel = torch.clamp(torch.randn(self.gtImageHR.size()).uniform_(0, 1.) * sigma  + 0., 0., 1.)
+    
+#         self.inputImage = self.gtImageHR + noiseModel
+        
+#         return self.inputImage, noiseModel
+
+
 import glob
 import numpy as np
 import time
 import cv2
+import random
+import torch
 from PIL import Image
 from torch.utils.data import DataLoader, Dataset
 import torchvision.transforms as transforms
@@ -9,6 +62,8 @@ from utilities.customUtils import *
 from dataTools.dataNormalization import *
 from dataTools.customTransform import *
 import os
+import pydicom
+
 class customDatasetReader(Dataset):
     def __init__(self, image_list, imagePath, height, width, transformation=True):
         self.image_list = image_list
@@ -21,26 +76,36 @@ class customDatasetReader(Dataset):
         self.mean = 0.0
         self.pov = 0.3
 
-
     def __len__(self):
         return (len(self.image_list))
     
     def __getitem__(self, i):
-
-
-
         self.transformHRGT = transforms.Compose([
                                                 transforms.ToTensor(),
                                                 self.normalize,
                                                 ])
         
         self.gtImageFileName = self.imagePath + extractFileName(self.image_list[i])
-        self.gtImage = Image.open(self.gtImageFileName)
+        
+        # Определяем, является ли файл DICOM
+        if self.gtImageFileName.lower().endswith('.dcm'):
+            print('ok')
+            # Чтение DICOM файла
+            dicom_file = pydicom.dcmread(self.gtImageFileName)
+            # Преобразование DICOM в массив numpy (предполагается изображение в градациях серого)
+            image_array = dicom_file.pixel_array
+            # Конвертация в PIL Image для дальнейших преобразований
+            tmp = Image.fromarray(image_array).convert('RGB')
+            tmp.save('tmp.png', 'PNG')
+            self.gtImage = Image.open('tmp.png')
+        else:
+            # Для не-DICOM файлов используется стандартная обработка
+            self.gtImage = Image.open(self.gtImageFileName)
 
-        # Transforms Images for training 
+        # Преобразование изображений для обучения
         self.gtImageHR = self.transformHRGT(self.gtImage)
         
-        #Noise Modeling
+        # Моделирование шума
         sigma = random.uniform(0, self.var ** self.pov)
         noiseModel = torch.clamp(torch.randn(self.gtImageHR.size()).uniform_(0, 1.) * sigma  + 0., 0., 1.)
     
--- main.py
+++ main.py
@@ -1,3 +1,47 @@
+# from utilities.parserUtils import *
+# from utilities.customUtils import *
+# from utilities.aestheticUtils import *
+# from dataTools.processDataset import *
+# from dataTools.patchExtractor import *
+# from mainModule.MIDRAN import *
+
+# if __name__ == "__main__":
+
+#     # Parsing Options
+#     options = mainParser(sys.argv[1:])
+#     if len(sys.argv) == 1:
+#         customPrint("Invalid option(s) selected! To get help, execute script with -h flag.")
+#         exit()
+    
+#     # Reading Model Configuration
+#     if options.conf:
+#         configCreator()
+
+#     # Loading Configuration
+#     config = configReader()
+  
+#     # Taking action as per received options
+#     if options.epoch:
+#         config=updateConfig(entity='epoch', value=options.epoch)
+#     if options.batch:
+#         config=updateConfig(entity='batchSize', value=options.batch)
+#     if options.manualUpdate:
+#         config=manualUpdateEntity()
+#     if options.modelSummary:
+#         MIDRAN(config).modelSummary()
+#     if options.train:
+#         MIDRAN(config).modelTraining(dataSamples=options.dataSamples)
+#     if options.retrain:
+#         MIDRAN(config).modelTraining(resumeTraning=True, dataSamples=options.dataSamples) 
+#     if options.inference:
+#         noiseSigmaSet = None
+#         if options.noiseSigma:
+#             noiseSigmaSet = options.noiseSigma.split(',')
+#             noiseSigmaSet = list(map(int, noiseSigmaSet))
+#         MIDRAN(config).modelInference(testImagesPath=options.sourceDir, outputDir=options.resultDir, noiseSet=noiseSigmaSet)
+#     if options.overFitTest:
+#         MIDRAN(config).modelTraining(overFitTest=True)
+
 from utilities.parserUtils import *
 from utilities.customUtils import *
 from utilities.aestheticUtils import *
@@ -6,9 +50,9 @@ from dataTools.patchExtractor import *
 from mainModule.MIDRAN import *
 
 if __name__ == "__main__":
-
     # Parsing Options
     options = mainParser(sys.argv[1:])
+    
     if len(sys.argv) == 1:
         customPrint("Invalid option(s) selected! To get help, execute script with -h flag.")
         exit()
@@ -22,11 +66,11 @@ if __name__ == "__main__":
   
     # Taking action as per received options
     if options.epoch:
-        config=updateConfig(entity='epoch', value=options.epoch)
+        config = updateConfig(entity='epoch', value=options.epoch)
     if options.batch:
-        config=updateConfig(entity='batchSize', value=options.batch)
+        config = updateConfig(entity='batchSize', value=options.batch)
     if options.manualUpdate:
-        config=manualUpdateEntity()
+        config = manualUpdateEntity()
     if options.modelSummary:
         MIDRAN(config).modelSummary()
     if options.train:
@@ -38,13 +82,10 @@ if __name__ == "__main__":
         if options.noiseSigma:
             noiseSigmaSet = options.noiseSigma.split(',')
             noiseSigmaSet = list(map(int, noiseSigmaSet))
+        # Check the input and output directories
+        if options.sourceDir is None or options.resultDir is None:
+            customPrint("Please specify source (-s) and result (-d) directories for inference.")
+            exit()
         MIDRAN(config).modelInference(testImagesPath=options.sourceDir, outputDir=options.resultDir, noiseSet=noiseSigmaSet)
     if options.overFitTest:
         MIDRAN(config).modelTraining(overFitTest=True)
-    
-        
-        
-        
-            
-
-
